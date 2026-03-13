9c2bb1dc4d5ac1ef537b5dc364279a77da4cdcb4
KarimAlaaEjada <kalaa@ejada.com>
2026-03-13 07:43:44 +0200

    Add CRUD endpoints and handlers for Product entity

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+    public class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+            return product is null ? throw new ProductNotFoundException() : new GetProductByIdResult(product);
+        }
+    }
+}
